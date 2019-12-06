270e9b9992c2050bca5b070709efc782488dd255
Björn <29443@ma-web.nl>
2019-12-06 14:22:13 +0100

    commit scott falco

--- 01_huisje/script.js
+++ 01_huisje/script.js
@@ -10,4 +10,37 @@ const height = window.innerHeight;
 canvas.width = width;
 canvas.height = height;
 
-// hier kun je op de canvas tekenen
+
+// context.beginPath();
+// context.lineWidth = "100";
+// context.strokeStyle = "red";
+// context.fillStyle = "grey";
+// context.arc(300,200,60,0,2*Math.PI);
+// context.closePath();
+// context.stroke();
+// context.fill();
+
+let circleObject = {};
+circleObject.x = 300;
+circleObject.y = 200;
+circleObject.radius = 60;
+
+circleObject.draw = function () {
+  context.beginPath();
+  context.arc(circleObject.x,circleObject.y,circleObject.radius,0,2*Math.PI);
+  context.closePath();
+  context.stroke();
+}
+
+circleObject.draw();
+circleObject.x = 400;
+circleObject.draw();
+
+function animate(){
+  context.clearRect(0,0,width,height);
+  circleObject.draw();
+  circleObject.x = circleObject.x + 1
+  circleObject.y = circleObject.y +1
+}
+
+setInterval(animate,1)
